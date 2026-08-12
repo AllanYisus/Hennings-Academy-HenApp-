b59f7f7adc2e60fb09ff02b673f87f7f3ff6d428
AllanYisus <allannotiracub@gmail.com>
2026-08-12 17:52:47 -0300

    Update app.config.js

--- js/core/app.config.js
+++ js/core/app.config.js
@@ -3,24 +3,20 @@
 // =========================================================
 
 window.HA = window.HA || {};
+    // 🔗 URLs OFICIAIS DO POWER AUTOMATE (EXCEL DATABASE)
 
 HA.Config = {
-    // 🔗 URLs OFICIAIS DO POWER AUTOMATE (EXCEL DATABASE)
-    
     // FLUJO 1 (GET - Leitor Maestro)
     URL_LER: "https://default863b40a279194b128e0e7678554bee.21.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/21/workflows/3068ae606a9d47c4967b39b43e2f157f/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vnTdxDEE1wSXgDnHWTgIVh-WIuVifc8RLNbj7Y4xTtw",
-    
     // FLUJO 2 (POST - Salvar Usuários)
     URL_SALVAR_USUARIO: "https://default863b40a279194b128e0e7678554bee.21.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/08/workflows/a6b77f5bb818479f986d0972cf739473/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=txK260vhFVS6RJ1hahS5VlH6eE0IReW47yqamNHZDhE",
-    
-    // FLUJO 3 (POST - Salvar Colaboradores)
+    // FLUJO 2 (POST - Salvar Usuários)
     URL_SALVAR_COLABORADOR: "https://default863b40a279194b128e0e7678554bee.21.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/02/workflows/1b1b45bc522943dea6a7030485f680f6/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FYOgLPg178X4sEFRmrgNYi9zsVwGFLpr05BkGbE6XtI",
-    
     // FLUJO 4 (POST - Salvar Treinamentos)
     URL_SALVAR_TREINAMENTO: "https://default863b40a279194b128e0e7678554bee.21.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/20/workflows/7beb1cb49aeb43ae97b3eebe11b4bbcd/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=K8zXArbmlM1roTfmd63pOKa54Enr8J7rzMVwQndiuao",
-
-    URL_UPLOAD_PDF: "",
-    URL_ENVIAR_EMAIL: ""
+    
+    URL_UPLOAD_PDF: "https://default863b40a279194b128e0e7678554bee.21.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/16/workflows/27dca153a40148bca7df16a7b1a1ec29/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=BVSBVhhjo48kk7dbAtqBxDiHyYdG1TrXr3oqhsGa5I8",
+    URL_ENVIAR_EMAIL: "https://default863b40a279194b128e0e7678554bee.21.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/25/workflows/b760991191ee4f929f9603aa31dfbe9b/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=dFd3cioggFgaY9yCxeMKroKFbMVNvIWZ6wijIh0w-tw"
 };
 
 HA.Constants = {
